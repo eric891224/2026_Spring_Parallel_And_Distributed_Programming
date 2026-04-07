91c69a3f2f820b8e096df0047f14cb57be7169c6
eric891224 <s96006730@gmail.com>
2026-04-07 20:19:09 +0800

    complete task 2

---
+++ hw2/test_serial.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+TESTCASE_ROOT="${TESTCASE_ROOT:-$HOME/testcases_hw2}"
+TESTCASE_NAME="${1:-test_small_uniform_50}"
+
+echo "Running spmv_serial with test case: $TESTCASE_NAME"
+
+./spmv_serial \
+    "$TESTCASE_ROOT/$TESTCASE_NAME.mtx" \
+    "$TESTCASE_ROOT/$TESTCASE_NAME.vec"
